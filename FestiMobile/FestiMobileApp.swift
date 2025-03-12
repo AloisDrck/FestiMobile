6214160ee474019ae583643caad7f37d670499a4
Aloïs Drucké <al.drucke@gmail.com>
2025-03-12 10:14:41 +0100

    First Commit, initialisation du projet

---
+++ FestiMobile/FestiMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  FestiMobileApp.swift
+//  FestiMobile
+//
+//  Created by Aloïs Drucké on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FestiMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FestiMobileTests/FestiMobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  FestiMobileTests.swift
+//  FestiMobileTests
+//
+//  Created by Aloïs Drucké on 12/03/2025.
+//
+
+import Testing
+@testable import FestiMobile
+
+struct FestiMobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
